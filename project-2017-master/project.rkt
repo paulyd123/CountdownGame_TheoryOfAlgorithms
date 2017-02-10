af5e727bdbe217b975cfe99dca28a64e0276858f
paulyd123 <g00297086@gmit.ie>
2017-02-10 16:01:16 +0000

    Random number generator
    Created a random number generator to generate a random number between
    100 and 1000 and then outputs it

--- project-2017-master/project.rkt
+++ project-2017-master/project.rkt
@@ -3,4 +3,16 @@
 ;Defining the list of numbers used to calculate the sum
 (define numbers(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 
-(define nums(list))
+;Outputs list of numbers defined above
+numbers
+
+;Random number generator that generates a number between the max and the min which is 100 to 1000
+(define rand(random 100 1000))
+
+;Outputs random number between 100 and 1000
+rand
+
+
+
+
+
