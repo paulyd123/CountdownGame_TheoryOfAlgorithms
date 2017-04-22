a77343e339471a834715d04cc081609a6f65353f
paulyd123 <g00297086@gmit.ie>
2017-04-22 22:07:55 +0100

    Added in comments and stack

--- project-2017-master/project.rkt
+++ project-2017-master/project.rkt
@@ -1,20 +1,5 @@
 #lang racket
 
-;Defining the list of numbers used to calculate the sum
-;(define numbers(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
-
-;Outputs list of numbers defined above
-;numbers
-
-;Random number generator that generates a number between the max and the min which is 100 to 1000
-;(define rand(random 101 1000))
-
-;Outputs random number between 100 and 1000
-;rand
-
-;This creates an empty list to hold the 6 numbers chosen from the new list of numbers
-;(define numChosen (list))
-
 ;Defines list
 ;(define start(list -1 -1 -1 -1 1 1 1 1))
 
@@ -43,8 +28,21 @@
 ;Defining the list of numbers used to calculate the sum
 (define numbers(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 
+;Outputs list of numbers defined above
+numbers
+
+;Random number generator that generates a number between the max and the min which is 100 to 1000
+(define rand(random 101 1000))
+
+;Outputs random number between 100 and 1000
+rand
+
+;This creates an empty list to hold the 6 numbers chosen from the new list of numbers
+(define numChosen (list))
+
 ;Defines list of operations needed
 (define ops ( list '+ '- '* '/)) ; ' = Tells to use symbol not function
+
 
 ;Used to calculate RPN, taken from https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#Racket
 (define (calculate-RPN expr)
@@ -56,16 +54,18 @@
      [('- (list x y s ___)) (cons (- y x) s)]
      [('* (list x y s ___)) (cons (* x y) s)]
      [('/ (list x y s ___)) (cons (/ y x) s)]
-     [('^ (list x y s ___)) (cons (expt y x) s)]
      [(x s) (error "calculate-RPN: Cannot calculate the expression:" 
                    (reverse (cons x s)))])))
 
 ;Applies function from above to each element of the list which in turn gets the valid rpn
 (define (valid-rpn? e (s 0)) ;default value is 0
   (if (null? e)
+      
       (if (= s 1) #t #f)
+      
       (if (= (car e) 1)
           (valid-rpn? (cdr e) (+ s 1))
+          
       (if (< s 2)
           #f
           (#t))))) ;true/false
